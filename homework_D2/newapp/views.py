970a0ded48d363f0e99abfc45e076ec0341a1d79
Roman <romansitdik@gmail.com>
2023-06-10 23:20:21 +0300

    Homework Module D3

--- homework_D2/newapp/views.py
+++ homework_D2/newapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+class NewsList(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news'
+    queryset = Post.objects.order_by('-id')
+class NewsDetail(DetailView):
+    model = Post
+    template_name = 'new.html'
+    context_object_name = 'new'
